31250fcc28c00fae7b6c80ba1c9836d03b534dcd
Andrew <andy.yanushkevich@gmail.com>
2018-09-14 19:08:01 +0300

    Add 'love-triangle' task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	length = preferences.length;
+	let loveTriangleNumber = 0;
+	let lovingNumber,belovedNumber;
+
+  for(let i = 0;i < preferences.length;i++){
+  	lovingNumber = preferences[i];
+  	
+  	for(let j = 0;j < 2;j++){
+  		belovedNumber = preferences[lovingNumber - 1];
+  		if(belovedNumber === lovingNumber){
+  			lovingNumber = -1;
+  			break;
+  		}
+  		lovingNumber = belovedNumber;
+  	}
+
+  	if(i === lovingNumber - 1){
+  		loveTriangleNumber++;
+  	}
+  }
+  return loveTriangleNumber / 3;
 };
